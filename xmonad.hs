dd0964d006cb4c78515909beb6d5414d3b279c79
Benjamin Brunzel <benjamin.brunzel@iteratec.de>
2017-10-24 09:46:55 +0200

    Rework xmonad layouts

--- xmonad.hs
+++ xmonad.hs
@@ -80,10 +80,21 @@ myManageHook = composeAll
 -- The available layouts.  Note that each layout is separated by |||,
 -- which denotes layout choice.
 --
-myLayout = avoidStruts (
-    Tall 1 (3/100) (1/2) |||
-    Mirror (Tall 1 (3/100) (1/2)) |||
-    noBorders (fullscreenFull Full))
+myLayout = tiled ||| Mirror tiled ||| full
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
+
+     -- The default number of windows in the master pane
+     nmaster = 1
+
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
+
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3/100
+
+     full    = noBorders( fullscreenFull Full )
 
 ------------------------------------------------------------------------
 -- Colors and borders
@@ -257,7 +268,10 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
      sendMessage (IncMasterN (-1)))
 
   -- Toggle the status bar gap.
-  -- TODO: update this binding with avoidStruts, ((modMask, xK_b),
+  --  Use this binding with avoidStruts from Hooks.ManageDocks.
+  -- See also the statusBar function from Hooks.DynamicLog.
+  , ((modMask, xK_b),
+     sendMessage ToggleStruts)
 
   -- Quit xmonad.
   , ((modMask .|. shiftMask, xK_q),
@@ -343,6 +357,7 @@ main = do
           , ppSep = "   "
       }
       , manageHook = manageDocks <+> myManageHook
+      , layoutHook = avoidStruts  $  layoutHook defaultConfig
       , startupHook = setWMName "LG3D"
   }
 
